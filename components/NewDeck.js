aad3539696b24fb3055ff2d838602eea25bb367b
Renington Neri <renington.neri@gmail.com>
2017-12-26 20:26:20 -0200

    feat: Changes for newDeck. stateful component and submit function

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,19 +1,89 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Platform } from 'react-native'
 
-const NewDeck = () => {
-    return (
-        <View>
-            <Text style={styles.title}>New Deck</Text>
-        </View>
-    )
+import { saveDeckTitle, addCardToDeck  } from '../utils/api'
+import { red, white } from '../utils/colors'
+
+class NewDeck extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            deckTitle: '',
+            error: ''
+        }
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    onSubmit() {
+        if(this.state.deckTitle !== ''){
+            // saveDeckTitle(this.state.deckTitle)
+            console.log('save',this.state.deckTitle)
+
+            this.props.navigation.navigate(
+                'DeckDetail',
+                // {name: this.state.text}
+            )
+
+            this.setState({ deckTitle: '' })
+        }else{
+            this.setState({ error: 'Input empty. Type a text.' })
+        }
+    }
+
+    render() {
+        return (
+            <View>
+                <Text style={styles.title}>What is the title of your new deck?</Text>
+                <TextInput
+                    style={{height: 50,width:150, fontSize: 12, borderColor: 'gray', borderWidth: 1, padding: 10}}
+                    value={this.state.deckTitle}
+                    onChangeText={ (text) => this.setState({deckTitle: text, error: ''}) }
+                    placeholder="Deck Name"
+                    />
+                    <Text style={styles.error}>{this.state.error}</Text>
+                <TouchableOpacity style={ Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.androidSubmitBtn } onPress={this.onSubmit} >
+                    <Text style = {styles.submitBtnText}>ADD DECK</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
 }
+
+
 
 const styles = StyleSheet.create({
     title: {
         fontSize: 20,
         paddingTop: 20,
         paddingBottom: 20
+    },
+    error: {
+        fontSize: 10,
+        color: red
+    },
+    iosSubmitBtn: {
+        backgroundColor: red,
+        padding: 10,
+        borderRadius: 7,
+        height: 45,
+        marginLeft: 40,
+        marginRight: 40,
+    },
+    AndroidSubmitBtn: {
+        backgroundColor: red,
+        padding: 10,
+        paddingLeft: 30,
+        paddingRight: 30,
+        height: 45,
+        borderRadius: 2,
+        alignSelf: 'flex-end',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    submitBtnText: {
+        color: white,
+        fontSize: 22,
+        textAlign: 'center',
     },
   });
 
