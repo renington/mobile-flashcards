0a3294b50211120916ec32f3a912d9e291e83556
Renington Neri <renington.neri@gmail.com>
2017-12-26 22:23:33 -0200

    feat: Changes for DeckList

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,19 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { getDecks } from '../utils/api'
 
-const DeckList = () => {
-    return (
-        <View>
-            <View>
-                <Text style={styles.title}>Title Test</Text>
-                <Text>(5) cards.</Text>
-            </View>
-            <View>
-                <Text style={styles.title}>Title Test</Text>
-                <Text>(5) cards.</Text>
-            </View>
-        </View>
-    )
+class DeckList extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            decks: null
+        }
+    }
+
+    componentDidMount() {
+        getDecks().then((decks) => {
+            this.setState({ decks: JSON.parse(decks) })
+        })
+    }
+
+    render() {
+        const { decks } = this.state
+
+        if(decks !== null){
+            return (
+                <View>
+                    {Object.keys(decks).map((deck, index) => {
+                        return (
+                            <View key={index}>
+                                <Text style={styles.title}>{deck}</Text>
+                                <Text>({decks[deck].questions.length}) cards</Text>
+                            </View>
+                        )
+                    })}
+                </View>
+            )
+        }else{
+            return <Text>Loading...</Text>
+        }
+    }
 }
 
 const styles = StyleSheet.create({
--- utils/api.js
+++ utils/api.js
@@ -17,7 +17,13 @@ export function getDecks() {
 }
 
 export function getDeck() {
-
+    // return AsyncStorage.getItem(CALENDAR_STORAGE_KEY)
+    // .then((results) => {
+    //   const data = JSON.parse(results)
+    //   data[key] = undefined
+    //   delete data[key]
+    //   AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data))
+    // })
 }
 
 export function addCardToDeck() {
