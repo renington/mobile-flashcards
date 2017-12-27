b02f70c4570e5e45bf7269e736fb99133098ae2a
Renington Neri <renington.neri@gmail.com>
2017-12-27 02:40:39 -0200

    feat: Creating DeckDetails Component

--- components/DeckDetail.js
+++ components/DeckDetail.js
@@ -1,19 +1,84 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, Platform } from 'react-native'
 
-const DeckDetail = () => {
-    return (
-        <View>
-            <Text style={styles.title}>Deck Details</Text>
-        </View>
-    )
+import { getDeck } from '../utils/api'
+import { red, white } from '../utils/colors'
+
+class DeckDetail extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            deck: null
+        }
+    }
+
+    
+    componentDidMount() {
+        const { title } = this.props.navigation.state.params
+
+        getDeck(title).then((deck) => {
+            this.setState({ deck: deck })
+        })
+    }
+    
+    render(){
+        const { deck } = this.state
+
+        if(deck !== null){
+            return (
+                <View>
+                    <Text style={styles.title}>{deck.title}</Text>
+                    <Text style={[styles.title, {fontSize: 14, paddingTop: 0, paddingBottom: 10}]}>({deck.questions.length}) cards</Text>
+        
+                    <TouchableOpacity style={ Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.androidSubmitBtn } >
+                        <Text style = {styles.submitBtnText}>ADD CARD</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={ Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.androidSubmitBtn } >
+                        <Text style = {styles.submitBtnText}>START QUIZ</Text>
+                    </TouchableOpacity>
+                </View>
+            )
+        }else{
+            return <Text>Loading ...</Text>
+        }
+    }
 }
 
 const styles = StyleSheet.create({
     title: {
         fontSize: 20,
         paddingTop: 20,
-        paddingBottom: 20
+        paddingBottom: 10,
+        textAlign: 'center'
+    },
+    iosSubmitBtn: {
+        backgroundColor: red,
+        padding: 10,
+        borderRadius: 7,
+        height: 45,
+        marginLeft: 40,
+        marginRight: 40,
+        marginTop: 10,
+        marginBottom: 10,
+    },
+    AndroidSubmitBtn: {
+        backgroundColor: red,
+        padding: 10,
+        paddingLeft: 30,
+        paddingRight: 30,
+        height: 45,
+        borderRadius: 2,
+        alignSelf: 'flex-end',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10,
+        marginBottom: 10,
+    },
+    submitBtnText: {
+        color: white,
+        fontSize: 22,
+        textAlign: 'center',
     },
   });
 
