e02d6347e60889264ac65b7e23062bd1157b6a24
Renington Neri <renington.neri@gmail.com>
2017-12-27 02:39:57 -0200

    refac: Fix promisses and changes for getDeck

--- utils/api.js
+++ utils/api.js
@@ -1,4 +1,5 @@
 import { AsyncStorage } from 'react-native'
+
 export const MOBILE_FLASHCARD_STORAGE_KEY = 'mobile-flashcads:flashCard'
 
 export function saveDeckTitle(deckTitle) {
@@ -11,19 +12,18 @@ export function saveDeckTitle(deckTitle) {
 }
 
 export function getDecks() {
-    return AsyncStorage.getItem(MOBILE_FLASHCARD_STORAGE_KEY, (results) => {
-        return JSON.parse(results)
-    })
+    return AsyncStorage.getItem(MOBILE_FLASHCARD_STORAGE_KEY)
+        .then((results) => {
+            return JSON.parse(results)
+        })
 }
 
-export function getDeck() {
-    // return AsyncStorage.getItem(CALENDAR_STORAGE_KEY)
-    // .then((results) => {
-    //   const data = JSON.parse(results)
-    //   data[key] = undefined
-    //   delete data[key]
-    //   AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data))
-    // })
+export function getDeck(title) {
+    return AsyncStorage.getItem(MOBILE_FLASHCARD_STORAGE_KEY)
+        .then((results) => {
+            const decks = JSON.parse(results)
+            return (decks[title])
+        })
 }
 
 export function addCardToDeck() {
